9b88e4c4888eacde834963ce4028a70d1ad4f88c
codingwolf-at <atameshwari9@gmail.com>
2020-12-28 22:14:38 +0530

    fix: output table working

--- main.js
+++ main.js
@@ -4,6 +4,8 @@ const inputBill = document.querySelector('#inputBill');
 const inputCash = document.querySelector('#inputCash');
 // ref to give change button
 const giveChangeButton = document.querySelector('#button');
+// ref to output table
+const outputTable = document.querySelector('#output');
 
 // init array of available notes
 const availableNotes = [2000, 500, 100, 20, 10, 5, 1];
@@ -17,16 +19,41 @@ giveChangeButton.addEventListener("click", calculateChange);
 function calculateChange() {
     const inputBillAmount = inputBill.value;
     const inputCashAmount = inputCash.value;    
+    const changeArray = [];
     let change = inputCashAmount - inputBillAmount;
 
     availableNotes.map(note => {
         if(note <= change) {
             var numOfNotes = parseInt(change/note);
+            changeArray.push(numOfNotes);
             change = change - (note*numOfNotes);
-            console.log("remaining", change)
-            console.log(note + " x " + numOfNotes);
+        } else {
+            changeArray.push(0);
         }
-    })
+    });
+    // show table
+    renderList(availableNotes, changeArray);
+}
 
-    // console.log(inputBillAmount, inputCashAmount, change);
+// function to render list
+function renderList(availableNotes, changeArray) {
+    // getting the data in tabular form via looping
+    const cashRegisterHTML = [];  
+    for (let i = 0; i<7; i++) {
+        cashRegisterHTML.push(createListItem(availableNotes[i], changeArray[i]));
+    }
+    const joinedCashRegisterHTML = cashRegisterHTML.join("")
+    // setting the html of table
+    outputTable.innerHTML = joinedCashRegisterHTML; 
+}
+
+// function to create HTML for list 
+function createListItem(availableNotes, changeArray) {
+    return `
+    <tr>
+    <th scope="row">${availableNotes}</th>
+    <td>x</td>
+    <td>${changeArray}</td>
+    </tr>
+    `;
 }
